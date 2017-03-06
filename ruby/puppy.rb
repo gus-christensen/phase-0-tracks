c938e5d56948e5d3aeb9019722249b950c40628a
gus-christensen <augustjc@gmail.com>
2017-03-05 22:56:36 -0500

    add actual code

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,17 +1,29 @@
 #the puppy class should be very focused on sculpting a viable adult dog.
-#
+#or it should be very, very cute. either/or
+class Puppy
+#getting the puppy is the absolute cutest time
+#though is it also the pup's most vulnerable moment IN LIFE.
+	def initialize
+		puts "Im your new mom and I'll love you the most!"
+	end
 #for instance, a method called #peeing_in_the_house,
 #the owner should IMMEDIATELY respond, catching the pup in the act
 #to properly correct the pup, DURING the bad act.
 #followed by a walk outside/proper dissemination of discipline-related stress
-#
+	def accident
+		puts "oh tiny baby! you're peeing! !!!NO!!! ::physically move the pups rear to add discomfort, forcing cessation of accident:: ::take the pup outside!:: ::have a lovely walk::"
+	end
 #similarly, a chewing or teething method
 #would prompt IMMEDIATE response from the owner
 #startling the pup while in the act
 #followed by providing a replacement chewing option
 #and de-stressing.
-#
+	def chewing
+		puts "oh my sweetest! you're destroying my thing! !!!NO!!! ::act super scary:: ::maybe even flip the pup onto it's back:: ::growling is allowed:: ::suggest an alternative chew toy:: ::let the pup believe you are still angry for 2-30min depending on the severity of the incident:: ::be BEST FRIENDS again, make sure your dog knows that you love it more than anything else in the world::"
+	end
 #Leash training method is much much more involved.
+
+end
 #
 #
 #
