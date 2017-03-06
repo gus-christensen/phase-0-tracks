8c193d4f1714a68e7e2ee3a8ec57377c6c32a6a9
gus-christensen <augustjc@gmail.com>
2017-03-06 11:23:15 -0500

    still working on driver code and rspec

---
+++ ruby/GAME_TIME/game_spec.rb
@@ -0,0 +1,53 @@
+
+require_relative "game"
+
+describe HangedGame do
+	let(:hangedgame) {HangedGame.new}
+
+	it "accepts a word or phrase from user1" do
+		expect(game. ( )).to eq 
+
+	it "" do
+		expect(game. ( )).to eq 
+
+	it "" do
+		expect(game. ( )).to eq 
+
+	it "" do
+		expect(game. ( )).to eq 
+
+	it "" do
+		expect(game. ( )).to eq 
+
+#One user can enter a word (or phrase, 
+#if you would like your game to support that), 
+#and another user attempts to guess the word.
+#--game should set all input text to lowercase 
+#--and check that the string only contains letters and spaces.
+#--prompt again if string contains numbers/punctuation/etc
+#--the class's initialize input is the first user's string.
+#--the input should either be taken in password format.. 
+#--or the terminal should be cleared after initializing.
+
+#Guesses are limited, 
+#and the number of guesses available is related to the length of the word.
+
+
+#Repeated guesses do not count against the user.
+#--each guess is found and deleted from an alphabet array.
+#--if the letter has already been deleted from the array, 
+#--prompt for a new guess; "you already guessed that!"
+#--the length of the mapped alphabet array will determine if user2 has run out of guesses.
+
+#The guessing player receives continual feedback on the current state of the word. 
+#So if the secret word is "unicorn", 
+#the user will start out seeing something like "_ _ _ _ _ _ _", 
+#which would become "_ _ _ c _ _ _" after the user enters a guess of "c".
+#--input string is split into an array. 
+#--the feedback display checks if each letter in the new array still exists in the guessing pool,
+#--if the letter is still in the alphabet array, it is substituted by "_"
+
+#The user should get a congratulatory message if they win, 
+#and a taunting message if they lose.
+#--when user2 runs out of guesses. game over.
+#--when none of the letters in the secret word array are still remaining in alphabet array.
