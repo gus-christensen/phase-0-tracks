08a81a3300702708abec1d308b01a7f4428a41db
gus-christensen <augustjc@gmail.com>
2017-03-06 16:13:13 -0500

    add arrays for horse names and colors

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colorArray = ["white", "fawn", "black", "gray"];
+var horseNames = ["Coco", "Tilly", "Jupiter", "Ed"];
+
+colorArray.push("brown");
+horseNames.push("Teal");
+
+console.log(colorArray);
+console.log(horseNames);
